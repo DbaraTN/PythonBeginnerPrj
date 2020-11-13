85dcf2f8aba545acdf464565b56aa389f32caaf6
dbaram <muhammed.dbara@gmail.com>
2020-11-13 23:34:02 +0100

    added program

--- random_name.py
+++ random_name.py
@@ -1,1 +1,12 @@
+import string
 import random
+
+
+def name_gen():
+    l1 = random.choice(string.ascii_uppercase)
+    l2 = random.choice(string.ascii_lowercase)
+    l3 = random.choice(string.ascii_lowercase)
+    l4 = random.choice(string.ascii_lowercase)
+    return l1+l2+l3+l4
+
+print(name_gen())
