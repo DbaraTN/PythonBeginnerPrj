0aaab61e8d641d43cc6520309117f8747bfdf1a9
dbaram <muhammed.dbara@gmail.com>
2020-11-14 15:02:49 +0100

    Added user control for the name length

--- random_name.py
+++ random_name.py
@@ -1,10 +1,10 @@
 import string
 import random
 
-
+name_lenth = int(input('Enter length of the name:'))
 def name_gen():
     name = ''
-    for i in range(4):
+    for i in range(name_lenth):
         if i == 0:
             name = name+random.choice(string.ascii_uppercase)
         else:
