3cab236fc1045a1989f450e06fba7ea56dd11af7
dbaram <muhammed.dbara@gmail.com>
2020-11-14 14:59:41 +0100

    added loop for

--- random_name.py
+++ random_name.py
@@ -3,10 +3,12 @@ import random
 
 
 def name_gen():
-    l1 = random.choice(string.ascii_uppercase)
-    l2 = random.choice(string.ascii_lowercase)
-    l3 = random.choice(string.ascii_lowercase)
-    l4 = random.choice(string.ascii_lowercase)
-    return l1+l2+l3+l4
+    name = ''
+    for i in range(4):
+        if i == 0:
+            name = name+random.choice(string.ascii_uppercase)
+        else:
+            name = name+random.choice(string.ascii_lowercase)
+    return name
 
 print(name_gen())
